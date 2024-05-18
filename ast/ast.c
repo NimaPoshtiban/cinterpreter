9c136ccfbe7a19885548a8dddb98aaa141184b0c
Nima Poshtiban <Poshtibannima@gmail.com>
2024-05-18 11:16:14 +0330

    fix syntax errors and expand ast functions

--- ast/ast.c
+++ ast/ast.c
@@ -1,31 +1,103 @@
 #include "ast.h"
-#include <stdlib.h>
 #include <string.h>
-#define BUFSIZE 256
-size_t strlcpy(char *dest, const char *src, size_t n) {
-  size_t len = strlen(src);
-
-  if (len < n)
-    memcpy(dest, src, len + 1);
-  else {
-    memcpy(dest, src, n - 1);
-    dest[n - 1] = '\0';
-  }
-
-  return len;
+char* ast_identifier_token_literal(ast_identifier* ident) {
+	return ident->token.literal;
+}
+char* ast_identifier_string(ast_identifier* ident) {
+	return ident->value;
 }
 
-char *ast_token_literal(ast_program *program) {
-  if (_countof(program->statements) > 0) {
-    return program->statements->node->ast_token_literal();
-  }
-  return "";
+ast_identifier ast_identifier_init(tk_token token, char* value)
+{
+	ast_identifier identifier = {token,value,ast_identifier_token_literal,ast_identifier_string};
+	
+	return identifier;
 }
 
-char *ast_string(ast_program *program) {
-  char *buf = malloc(sizeof(char) * BUFSIZE);
-  for (size_t i = 0; i < _countof(program->statements); i++) {
-    strlcpy(buf, program->statements[i].node->ast_string(), sizeof(buf));
-  }
-  return buf;
+char* ast_let_statement_token_literal(ast_let_statement* statement)
+{
+	return statement->token.literal;
 }
+
+
+char* ast_let_statement_string(ast_let_statement* statement)
+{
+	char str[AST_MAX_BUFFER_SIZE];
+	strcat(str,statement->token_literal(statement));
+	strcat(str, " ");
+	strcat(str, statement->name.string(&statement->name));
+	strcat(str, " = ");
+	if (&statement->value != NULL)
+	{
+		strcat(str,statement->value.node.string(&statement->value));
+	}
+	strcat(str, ";\0");
+	return str;
+}
+
+ast_let_statement ast_let_statement_init(tk_token token, ast_identifier name, ast_expression value)
+{
+	ast_let_statement let = { token,name,value,ast_let_statement_token_literal,ast_let_statement_string };
+}
+
+char* ast_return_statement_token_literal(ast_return_statement* statement)
+{
+	return statement->token_literal;
+}
+
+char* ast_return_statement_string(ast_return_statement* statement)
+{
+	char str[AST_MAX_BUFFER_SIZE];
+	strcat(str, statement->token_literal(statement));
+	strcat(str, " ");
+	if (&statement->return_value != NULL)
+	{
+		strcat(str, statement->return_value.node.string(&statement->return_value));
+	}
+	strcat(str, ";");
+	return str;
+}
+
+ast_return_statement ast_return_statement_init(tk_token token, ast_expression return_value)
+{
+	ast_return_statement rt = { token,return_value,ast_return_statement_token_literal,ast_return_statement_string };
+	return rt;
+}
+
+char* ast_expression_statement_token_literal(ast_expression_statement* expr)
+{
+	return expr->token_literal;
+}
+
+char* ast_expression_statement_string(ast_expression_statement* expr)
+{
+	if (&expr->expression != NULL) {
+		return (char*)expr->expression.node.string(&expr->expression);
+	}
+	return "\0";
+}
+
+ast_expression_statement ast_expression_statement_init(tk_token token, ast_expression expr)
+{
+	ast_expression_statement expression = { token,expr,ast_expression_statement_token_literal,ast_expression_statement_string };
+	return expression;
+}
+
+char* ast_integer_literal_token_literal(ast_integer_literal* il)
+{
+	return il->token_literal;
+}
+
+char* ast_integer_literal_string(ast_integer_literal* il)
+{
+	return il->token_literal;
+}
+
+ast_integer_literal ast_integer_literal_init(tk_token token, int64_t value)
+{
+	ast_integer_literal il = { token,value,ast_integer_literal_token_literal,ast_integer_literal_string };
+	return il;
+}
+
+
+
--- ast/ast.h
+++ ast/ast.h
@@ -1,21 +1,22 @@
 #pragma once
 #include "../token/tk_token.h"
 #include <stdint.h>
+#include <stdbool.h>
 #define AST_H
-
+#define AST_MAX_BUFFER_SIZE  1024
 typedef struct ast_node {
-	(char*)(*token_literal)(void*);
-	(char*)(*string)(void*);
+	char*(*token_literal)(void*);
+	char*(*string)(void*);
 }ast_node;
 
 typedef struct ast_expression {
 	ast_node node;
-	(void) (*expression_node)(void*);
+	void (*expression_node)(void*);
 } ast_expression;
 
 typedef struct ast_statement{
 	ast_node node;
-	(void)(*statement_node)(void*);
+	void(*statement_node)(void*);
 
 }ast_statement;
 
@@ -27,51 +28,46 @@ typedef struct ast_identifier
 {
 	tk_token token;
 	char* value;
-	(char*)(*token_literal)(ast_identifier*);
-	(char*)(*string)(ast_identifier*);
-	(void)(*expression_node)(ast_identifier*);
+	char*(*token_literal)(struct ast_identifier*);
+	char*(*string)(struct ast_identifier*);
 }ast_identifier;
 
 typedef struct ast_let_statement{
 	tk_token  token;
 	ast_identifier name;
 	ast_expression value;
-	(void)(*statement_node)(ast_let_statement*);
-	(char*)(*token_literal)(ast_let_statement*);
-	(char*)(*string)(ast_let_statement*);
+	char*(*token_literal)(struct ast_let_statement*);
+	char*(*string)(struct ast_let_statement*);
 }ast_let_statement;
 
 typedef struct ast_return_statement{
 	tk_token token;
 	ast_expression return_value;
-	(void)(*statement_node)(ast_return_statement*);
-	(char*)(*token_literal)(ast_return_statement*);
-	(char*)(*string)(ast_return_statement*);
+	char*(*token_literal)(struct ast_return_statement*);
+	char*(*string)(struct ast_return_statement*);
 }ast_return_statement;
 
 typedef struct ast_expression_statement{
 	tk_token token;
 	ast_expression expression;
-	(void)(*statement_node)(ast_expression_statement*);
-	(char*)(*token_literal)(ast_expression_statement*);
-	(char*)(*string)(ast_expression_statement*);
+	char*(*token_literal)(struct ast_expression_statement*);
+	char*(*string)(struct ast_expression_statement*);
 }ast_expression_statement;
 
 typedef struct ast_integer_literal {
 	tk_token token;
 	int64_t value;
-	(void)(*expressionNode)(ast_integer_literal*);
-	(char*)(*token_literal)(ast_integer_literal*);
-	(char*)(*string)(ast_integer_literal*);
+	char*(*token_literal)(struct ast_integer_literal*);
+	char*(*string)(struct ast_integer_literal*);
 }ast_integer_literal;
 
 typedef struct ast_prefix_expression {
 	tk_token token;
 	char* op; // operator
 	ast_expression right;
-	(void)(*expressionNode)(ast_prefix_expression*);
-	(char*)(*token_literal)(ast_prefix_expression*);
-	(char*)(*string)(ast_prefix_expression*);
+	void(*expressionNode)(struct ast_prefix_expression*);
+	char*(*token_literal)(struct ast_prefix_expression*);
+	char*(*string)(struct ast_prefix_expression*);
 } ast_prefix_expression;
 
 typedef struct ast_infix_expression {
@@ -79,34 +75,30 @@ typedef struct ast_infix_expression {
 	char* op; // operator
 	ast_expression left;
 	ast_expression right;
-	(void)(*expressionNode)(ast_infix_expression*);
-	(char*)(*token_literal)(ast_infix_expression*);
-	(char*)(*string)(ast_infix_expression*);
+	char*(*token_literal)(struct ast_infix_expression*);
+	char*(*string)(struct ast_infix_expression*);
 }ast_infix_expression;
 
 typedef struct ast_index_expression {
 	tk_token token;
 	ast_expression left;
 	ast_expression index;
-	(void)(*expressionNode)(ast_index_expression*);
-	(char*)(*token_literal)(ast_index_expression*);
-	(char*)(*string)(ast_index_expression*);
+	char*(*token_literal)(struct ast_index_expression*);
+	char*(*string)(struct ast_index_expression*);
 }ast_index_expression;
 
 typedef struct ast_boolean {
 	tk_token token;
 	bool value;
-	(void)(*expressionNode)(ast_boolean*);
-	(char*)(*token_literal)(ast_boolean*);
-	(char*)(*string)(ast_boolean*);
+	char*(*token_literal)(struct ast_boolean*);
+	char*(*string)(struct ast_boolean*);
 }ast_boolean;
 
 typedef struct ast_block_statement {
 	tk_token token;
 	ast_statement* statements;
-	(void)(*statement_node)(ast_block_statement*);
-	(char*)(*token_literal)(ast_block_statement*);
-	(char*)(*string)(ast_block_statement*);
+	char*(*token_literal)(struct ast_block_statement*);
+	char*(*string)(struct ast_block_statement*);
 }ast_block_statement;
 
 typedef struct ast_if_expression {
@@ -114,58 +106,72 @@ typedef struct ast_if_expression {
 	ast_expression condition;
 	ast_block_statement* consequence;
 	ast_block_statement* alternative;
-	(void)(*expressionNode)(ast_if_expression*);
-	(char*)(*token_literal)(ast_if_expression*);
-	(char*)(*string)(ast_if_expression*);
+	
+	char*(*token_literal)(struct ast_if_expression*);
+	char*(*string)(struct ast_if_expression*);
 }ast_if_expression;
 
 typedef struct ast_while_expression{
 	tk_token token;
 	ast_expression condition;
 	ast_block_statement* body;
-	(void)(*expressionNode)(ast_while_expression*);
-	(char*)(*token_literal)(ast_while_expression*);
-	(char*)(*string)(ast_while_expression*);
+	char*(*token_literal)(struct ast_while_expression*);
+	char*(*string)(struct ast_while_expression*);
 }ast_while_expression;
 
 typedef struct ast_function_literal {
 	tk_token token;
 	ast_identifier* parameters;
 	ast_block_statement* body;
-	(void)(*expressionNode)(ast_function_literal*);
-	(char*)(*token_literal)(ast_function_literal*);
-	(char*)(*string)(ast_function_literal*);
+	char*(*token_literal)(struct ast_function_literal*);
+	char*(*string)(struct ast_function_literal*);
 }ast_function_literal;
 
 typedef struct ast_call_expression {
 	tk_token token;
 	ast_expression function;
 	ast_expression* arguments;
-	(void)(*expressionNode)(ast_call_expression*);
-	(char*)(*token_literal)(ast_call_expression*);
-	(char*)(*string)(ast_call_expression*);
+	char*(*token_literal)(struct ast_call_expression*);
+	char*(*string)(struct ast_call_expression*);
 }ast_call_expression;
 
 typedef struct ast_string_literal {
 	tk_token token;
 	char* value; 
-	(void)(*expressionNode)(ast_string_literal*);
-	(char*)(*token_literal)(ast_string_literal*);
-	(char*)(*string)(ast_string_literal*);
+	char*(*token_literal)(struct ast_string_literal*);
+	char*(*string)(struct ast_string_literal*);
 }ast_string_literal;
 
 typedef struct ast_array_literal {
 	tk_token token;
 	ast_expression* elements;
-	(void)(*expressionNode)(ast_array_literal*);
-	(char*)(*token_literal)(ast_array_literal*);
-	(char*)(*string)(ast_array_literal*);
-};
+	char*(*token_literal)(struct ast_array_literal*);
+	char*(*string)(struct ast_array_literal*);
+}ast_array_literal;
 
 typedef struct ast_hash_literal{
 	tk_token token;
 	hashmap pairs;
-	(void)(*expressionNode)(ast_hash_literal*);
-	(char*)(*token_literal)(ast_hash_literal*);
-	(char*)(*string)(ast_hash_literal*);
+	char*(*token_literal)(struct ast_hash_literal*);
+	char*(*string)(struct ast_hash_literal*);
 }ast_hash_literal;
+
+char* ast_identifier_token_literal(ast_identifier* ident);
+char* ast_identifier_string(ast_identifier* ident);
+ast_identifier ast_identifier_init(tk_token token, char* value);
+
+char* ast_let_statement_token_literal(ast_let_statement* statement);
+char* ast_let_statement_string (ast_let_statement* statement);
+ast_let_statement ast_let_statement_init(tk_token token, ast_identifier name, ast_expression value);
+
+char* ast_return_statement_token_literal(ast_return_statement* statement);
+char* ast_return_statement_string(ast_return_statement* statement);
+ast_return_statement ast_return_statement_init(tk_token token,ast_expression return_value);
+
+char* ast_expression_statement_token_literal(ast_expression_statement* expr);
+char* ast_expression_statement_string(ast_expression_statement* expr);
+ast_expression_statement ast_expression_statement_init(tk_token token, ast_expression expr);
+
+char* ast_integer_literal_token_literal(ast_integer_literal* il);
+char* ast_integer_literal_string(ast_integer_literal* il);
+ast_integer_literal  ast_integer_literal_init(tk_token token, int64_t value);
